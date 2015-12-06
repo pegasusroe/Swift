dca2f1605f090db0a8dc6251fac72ded6243719e
pegasusroe <pegasusroe@gmail.com>
2015-12-06 20:09:34 +0800

    Update anyGenerator.swift

--- SequenceType/anyGenerator.swift
+++ SequenceType/anyGenerator.swift
@@ -5,3 +5,18 @@ var naturalNumbers = anyGenerator{ ++n }
 naturalNumbers.next()    // Optional(1)
 naturalNumbers.next()    // Optional(2)
 naturalNumbers.next()    // Optional(3)
+
+
+enum Dwarf: Int {
+    
+    case Bashful = 1, Happy, Sleepy, Sneezy
+    
+    static var members : [Dwarf] { 
+        var i = 1
+        return Array(anyGenerator{ Dwarf(rawValue: i++) })
+    }
+    
+}
+
+print(Dwarf.members)
+// [Dwarf.Bashful, Dwarf.Happy, Dwarf.Sleepy, Dwarf.Sneezy]
